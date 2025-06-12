dde4e95584e4afd15b426455395f346a95acb439
Sborrello <samborrello3@gmail.com>
2025-06-11 22:07:22 -0400

    adds login

--- popup.js
+++ popup.js
@@ -1,3 +1,11 @@
-document.getElementById("testBtn").addEventListener("click", () => {
-  console.log("Button clicked!");
+document.getElementById("loginBtn").addEventListener("click", () => {
+  chrome.runtime.sendMessage({ type: "login" }, (response) => {
+    if (response?.token) {
+      console.log("Access Token:", response.token);
+      alert("Logged in!");
+      // You can now use this token to make Spotify Web API calls
+    } else {
+      alert("Spotify login failed.");
+    }
+  });
 });
